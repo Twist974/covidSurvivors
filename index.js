8ce43a51bfd4d8f0e6e41a3e473aaef493418b72
gabrielle <gabrielle.kbidi@gmail.com>
2021-03-19 02:22:39 +0100

    adding habitudes-alimentaires/vie sociale css/html

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function test (){
+const btnGenererRepas = document.querySelector("#GénérerRepas");
 
-    
-    console.log("blabla");
+function Generer() {
+    let legIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(legIndex);
+    let fruIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(fruIndex);
+    let proIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(proIndex);
+    let desIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(desIndex);
+    let froIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(froIndex);
+    let fecIndex = Math.floor(Math.random() * 7) + 1;
+    console.log(fecIndex);
 }
+
+btnGenererRepas.addEventListener('click', function(){
+    Generer()
+});
