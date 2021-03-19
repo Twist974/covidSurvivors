6d7f0d64a7479d77860ee039ac7b88530bc544fd
gabrielle <gabrielle.kbidi@gmail.com>
2021-03-19 14:07:49 +0100

    index.js à modifier

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const btnGenererRepas = document.querySelector("#GénérerRepas");
+const btnGenererRepas = document.getElementById('GenererRepas');
 
-function Generer() {
-    let legIndex = Math.floor(Math.random() * 7) + 1;
+function generer() {
+  const tableauSelect = document.getElementsByTagName('select');
+
+  // je boucle sur chaque element de mon tableau
+  // pour chaque select dans mon tableau
+
+  for (let i = 0; i < tableauSelect.length; i++) {
+    // je cherche la taille de mon select
+    console.log(tableauSelect[i].length);
+
+    // je calcul un aleatoire entre 1 et la taille de mon select
+    let legIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(legIndex);
-    let fruIndex = Math.floor(Math.random() * 7) + 1;
+    let fruIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(fruIndex);
-    let proIndex = Math.floor(Math.random() * 7) + 1;
+    let proIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(proIndex);
-    let desIndex = Math.floor(Math.random() * 7) + 1;
+    let desIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(desIndex);
-    let froIndex = Math.floor(Math.random() * 7) + 1;
+    let froIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(froIndex);
-    let fecIndex = Math.floor(Math.random() * 7) + 1;
+    let fecIndex = Math.floor(Math.random() * tableauSelect.length) + 1;
     console.log(fecIndex);
-}
+  }
+    // je selection l'item correspondant a cet aleatoire.
 
-btnGenererRepas.addEventListener('click', function(){
-    Generer()
-});
+  /* let legIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(legIndex);
+    let fruIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(fruIndex);
+    let proIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(proIndex);
+    let desIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(desIndex);
+    let froIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(froIndex);
+    let fecIndex = Math.floor(Math.random() * select.length) + 1;
+    console.log(fecIndex); */
+
+/* Math.floor(Math.random() * select.length) + 1;
+
+ */
